6d643992f1247d9598a1b7620298231fe60d1675
amezousan <goesbyamezou@gmail.com>
2022-09-07 17:44:59 +0900

    fix: Update Docs & Commitlint Config

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # apply-commit-rules
 This is an example of how to apply your commit rules on Git system
+
+See instructions under the https://zenn.dev/amezousan/scraps/15688951759a8d
--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,18 @@
-module.exports = {extends: ['@commitlint/config-conventional']}
+module.exports = {
+    extends: ['@commitlint/config-conventional'],
+    // https://commitlint.js.org/#/reference-rules
+    rules: {
+        'subject-case': [2, 'always', 
+            [
+                'lower-case', // default
+                'upper-case', // UPPERCASE
+                'camel-case', // camelCase
+                'kebab-case', // kebab-case
+                'pascal-case', // PascalCase
+                'sentence-case', // Sentence case
+                'snake-case', // snake_case
+                'start-case' // Start Case
+            ]
+        ],
+    },
+}
